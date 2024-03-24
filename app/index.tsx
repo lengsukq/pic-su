527fa8ad14208101f08e2ba6958ec5f77ed0622e
Su <lengsukq@foxmail.com>
2024-03-24 13:42:21 +0800

    手机端样式优化

--- app/index.tsx
+++ app/index.tsx
@@ -1,139 +1,127 @@
 'use client'
 import {
-    CrownFilled,
+    CrownFilled, CrownOutlined,
     GithubFilled,
-    InfoCircleFilled,
+    InfoCircleFilled, InfoCircleOutlined, MergeCellsOutlined,
     PlusCircleFilled,
-    QuestionCircleFilled,
+    QuestionCircleFilled, QuestionCircleOutlined,
     SearchOutlined,
     SmileFilled,
-    TabletFilled,
+    TabletFilled, TabletOutlined, UserOutlined,
 } from '@ant-design/icons';
 const ProLayout = dynamic(
     () => import("@ant-design/pro-components").then((com) => com.ProLayout),
     { ssr: false }
 );
-import {Input} from 'antd';
+import {Button, Input, Result} from 'antd';
 import React, {useState} from 'react';
 import dynamic from "next/dynamic";
+import {PageContainer, ProCard} from '@ant-design/pro-components';
 
-export default function App({children,}: Readonly<{ children: React.ReactNode; }>)
+export default function App({children}: Readonly<{ children: React.ReactNode; }>)
 {
-    const [pathname, setPathname] = useState('/list/sub-page/sub-sub-page1');
-
+    const [pathname, setPathname] = useState('/welcome');
     return (
-        <ProLayout
-            collapsed={true}
-            collapsedButtonRender={false}
-            layout="side"
-            route={{
-                routes: [
-                    {
-                        path: '/welcome',
-                        name: '欢迎',
-                        icon: <SmileFilled/>,
-                        component: './Welcome',
-                    },
-                    {
-                        path: '/admin',
-                        name: '管理',
-                        icon: <CrownFilled/>,
-                        access: 'canAdmin',
-                        component: './Admin',
-                    },
-                    {
-                        path: '/list',
-                        name: '列表',
-                        icon: <TabletFilled/>,
-                        access: 'canAdmin',
-                        component: './Admin',
-                    },
-                ],
+        <div
+            id="test-pro-layout"
+            style={{
+                height: '100vh',
             }}
-            location={{
-                pathname,
-            }}
-            menu={{
-                type: 'group',
-                collapsedShowTitle: true,
-            }}
-            avatarProps={{
-                src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-                size: 'small',
-                title: '七妮妮',
-            }}
-            actionsRender={(props) => {
-                if (props.isMobile) return [];
-                return [
-                    props.layout !== 'side' ? (
-                        <div
-                            key="SearchOutlined"
-                            aria-hidden
+        >
+            <ProLayout
+                fixSiderbar
+                route={{
+                    path: '/',
+                    routes: [
+                        {
+                            path: '/admin',
+                            name: '管理页',
+                            icon: <CrownOutlined />,
+                            access: 'canAdmin',
+                            component: './Admin',
+                            routes: [
+                                {
+                                    path: '/admin/sub-page1',
+                                    name: '一级页面',
+                                    icon: <CrownOutlined />,
+                                    component: './Welcome',
+                                },
+                                {
+                                    path: '/admin/sub-page2',
+                                    name: '二级页面',
+                                    icon: <CrownOutlined />,
+                                    component: './Welcome',
+                                },
+                                {
+                                    path: '/admin/sub-page3',
+                                    name: '三级页面',
+                                    icon: <CrownOutlined />,
+                                    component: './Welcome',
+                                },
+                            ],
+                        },
+                        {
+                            name: '列表页',
+                            icon: <TabletOutlined />,
+                            path: '/list',
+                            component: './ListTableList',
+                            routes: [
+                                {
+                                    path: '/list/sub-page2',
+                                    name: '二级列表页面',
+                                    icon: <CrownOutlined />,
+                                    component: './Welcome',
+                                },
+                                {
+                                    path: '/list/sub-page3',
+                                    name: '三级列表页面',
+                                    icon: <CrownOutlined />,
+                                    component: './Welcome',
+                                },
+                            ],
+                        },
+                    ],
+                }}
+                location={{
+                    pathname,
+                }}
+                avatarProps={{
+                    icon: <UserOutlined />,
+                    size: 'small',
+                    title: '七妮妮',
+                }}
+                actionsRender={() => [
+                    <InfoCircleOutlined key="InfoCircleOutlined" />,
+                    <QuestionCircleOutlined key="QuestionCircleOutlined" />,
+                    <MergeCellsOutlined key="MergeCellsOutlined" />,
+                ]}
+                menuFooterRender={(props) => {
+                    if (props?.collapsed) return undefined;
+                    return (
+                        <p
                             style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                marginInlineEnd: 24,
-                            }}
-                            onMouseDown={(e) => {
-                                e.stopPropagation();
-                                e.preventDefault();
+                                textAlign: 'center',
+                                color: 'rgba(0,0,0,0.6)',
+                                paddingBlockStart: 12,
                             }}
                         >
-                            <Input
-                                style={{
-                                    borderRadius: 4,
-                                    marginInlineEnd: 12,
-                                    backgroundColor: 'rgba(0,0,0,0.03)',
-                                }}
-                                prefix={
-                                    <SearchOutlined
-                                        style={{
-                                            color: 'rgba(0, 0, 0, 0.15)',
-                                        }}
-                                    />
-                                }
-                                placeholder="搜索方案"
-                                variant="borderless"
-                            />
-                            <PlusCircleFilled
-                                style={{
-                                    color: 'var(--ant-primary-color)',
-                                    fontSize: 24,
-                                }}
-                            />
-                        </div>
-                    ) : undefined,
-                    <InfoCircleFilled key="InfoCircleFilled"/>,
-                    <QuestionCircleFilled key="QuestionCircleFilled"/>,
-                    <GithubFilled key="GithubFilled"/>,
-                ];
-            }}
-            menuFooterRender={(props) => {
-                if (props?.collapsed) return undefined;
-                return (
-                    <p
-                        style={{
-                            textAlign: 'center',
-                            paddingBlockStart: 12,
+                            Power by Ant Design
+                        </p>
+                    );
+                }}
+                onMenuHeaderClick={(e) => console.log(e)}
+                menuItemRender={(item, dom) => (
+                    <a
+                        onClick={() => {
+                            setPathname(item.path || '/welcome');
                         }}
                     >
-                        Power by Ant Design
-                    </p>
-                );
-            }}
-            onMenuHeaderClick={(e) => console.log(e)}
-            menuItemRender={(item, dom) => (
-                <a
-                    onClick={() => {
-                        setPathname(item.path || '/welcome');
-                        console.log('item--',item.path)
-                    }}
-                >
-                    {dom}
-                </a>
-            )}
-        >
-            {children}
-        </ProLayout>
+                        {dom}
+                    </a>
+                )}
+            >
+                {children}
+            </ProLayout>
+        </div>
     );
 };
