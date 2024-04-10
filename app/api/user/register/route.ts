47c2822fdc58101c8afde9b49786ffbf9a9b5f95
lengsukq <lengsukq@foxmail.com>
2024-04-10 14:47:05 +0800

    注册接口

---
+++ app/api/user/register/route.ts
@@ -0,0 +1,28 @@
+'use server'
+import BizResult from '@/utils/BizResult';
+import {query} from "@/utils/db";
+export async function POST(req:Request) {
+    console.log('进入')
+    try {
+        const jsonData = await req.json();
+        const {username, hashedPassword, email} = jsonData;
+        console.log('{username, hashedPassword, email}',username, hashedPassword, email)
+        // 创建用户
+        // 检查用户名或邮箱是否已被占用
+        const existingUser = await query('SELECT id FROM users WHERE username = $1 OR email = $2', [username, email]);
+        // !是非空断言
+        if (existingUser.rowCount !> 0) {
+            // 用户名或邮箱已存在
+            return Response.json(BizResult.fail('', '户名或邮箱已被占用'))
+        }
+        const result = await query(
+            'INSERT INTO users(username, password, email, created_at, updated_at) VALUES($1, $2, $3, NOW(), NOW()) RETURNING id',
+            [username, hashedPassword, email]
+        );
+
+        return Response.json(BizResult.success('', '创建账号成功'))
+    } catch (error) {
+        console.log(error);
+        return Response.json(BizResult.fail('', '系统异常'))
+    }
+}
---
+++ utils/db.ts
@@ -0,0 +1,26 @@
+// db.ts
+import { Pool } from 'pg';
+
+// 创建 PostgreSQL 连接池
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+});
+
+// 查询函数，可以在你的应用中任何需要进行数据库查询的地方使用
+export async function query(text: string, params?: any[]) {
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log('executed query', { text, duration, rows: res.rowCount });
+    return res;
+}
+
+// 连接池关闭函数
+export async function closePool() {
+    await pool.end();
+}
+
