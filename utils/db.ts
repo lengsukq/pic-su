9f4e39a78bfff132689557ff58e01bf003398275
lengsukq <lengsukq@foxmail.com>
2024-05-14 09:14:58 +0800

    无db_port适配

--- utils/db.ts
+++ utils/db.ts
@@ -1,14 +1,23 @@
 // db.ts
 import { Pool } from 'pg';
+interface Config {
+    user: string;
+    host: string;
+    database: string;
+    password: string;
+    port?: number;
+}
 
+let configs: Config = {
+    user: process.env.DB_USER as string,
+    host: process.env.DB_HOST as string,
+    database: process.env.DB_NAME as string,
+    password: process.env.DB_PASSWORD as string,
+};
+// 如果process.env.DB_PORT存在则使用process.env.DB_PORT，否则使用默认值5432
+if (process.env.DB_PORT) configs={...configs, port: parseInt(process.env.DB_PORT, 10)};
 // 创建 PostgreSQL 连接池
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-});
+const pool = new Pool(configs);
 
 // 查询函数，可以在你的应用中任何需要进行数据库查询的地方使用
 export async function query(text: string, params?: any[]) {
