49c51e3e2e270b4d535a8dddc918c9c2ab123054
lengsukq <lengsukq@foxmail.com>
2024-05-20 11:08:16 +0800

    token 图片上传相册表修改

--- app/api/image-api/token/route.ts
+++ app/api/image-api/token/route.ts
@@ -1,19 +1,20 @@
 'use server'
 import BizResult from "@/utils/BizResult";
 import {upImgMain} from "@/utils/imageTools";
-import { NextRequest } from 'next/server'
+import {NextRequest} from 'next/server'
 import {query} from "@/utils/db";
 
-export async function POST(req:NextRequest) {
+export async function POST(req: NextRequest) {
     try {
         //multipart/form-data;
         const formData = await req.formData();
-        const token:string = <string>formData.get('token');
-        const file:File = <File>formData.get('file');
-        const base64 :string= <string>formData.get('base64');
+        const token: string = <string>formData.get('token');
+        const file: File = <File>formData.get('file');
+        const albumId = <string>formData.get('albumId');
+        const base64: string = <string>formData.get('base64');
         const bedType = <"SM" | "BilibiliDaily" | "BilibiliCover" | "IMGBB">formData.get('bedType') || process.env.NEXT_PUBLIC_DEFAULT_BED;
         // 参数有效性检查
-        if (!bedType || !token) {
+        if (!bedType || !token || !albumId) {
             // 参数不完整
             return BizResult.validateFailed('', '参数不完整');
         }
@@ -24,34 +25,43 @@ export async function POST(req:NextRequest) {
             [token]
         );
         // console.log('token 存在', result.rows);
-
-        if (result.rows[0]){
-            const info = result.rows[0];
-            // 日期判断
-            const expiresAtString = info.expires_at;
-            // 将字符串转换为Date对象
-            const expiresAt = new Date(expiresAtString);
-            // 获取当前日期
-            const currentDate = new Date();
-            console.log('当前日期', currentDate,expiresAt)
-            if (currentDate > expiresAt)  return BizResult.fail("token已失效");
-            if (info.status!=='enable')  return BizResult.fail("token已禁用");
-            if (info.usage_limit<=0)  return BizResult.fail("token次数已用完");
-
-        }else{
+        if (!result.rows.length) {
             return BizResult.authfailed('', 'token不存在');
         }
-        const fileData = {file, base64,bedType}
+        const info = result.rows[0];
+        // 日期判断
+        const expiresAtString = info.expires_at;
+        // 将字符串转换为Date对象
+        const expiresAt = new Date(expiresAtString);
+        // 获取当前日期
+        const currentDate = new Date();
+        console.log('当前日期', currentDate, expiresAt)
+        if (currentDate > expiresAt) return BizResult.fail("token已失效");
+        if (info.status !== 'enable') return BizResult.fail("token已禁用");
+        if (info.usage_limit <= 0) return BizResult.fail("token次数已用完");
+        // album_permissions为[]时，不限制上传相册id
+        if (info.album_permissions.length!==0){
+            if (!info.album_permissions.includes(Number(albumId))) {
+                console.log('token无权限上传该相册',info.album_permissions,albumId)
+                return BizResult.fail("token无权限上传该相册");
+            }
+        }
+        const fileData = {file, base64, bedType}
         // console.log('图片上传接口',fileData)
         const {msg, url} = await upImgMain(fileData);
         // 上传失败返回失败信息
-        if (msg!=='上传成功'){
+        if (msg !== '上传成功') {
             return BizResult.fail(msg);
         }
         // 更新使用的token，tokens表中的usage_limit 减1 current_usage加1
         await query(
             `UPDATE tokens SET usage_limit = usage_limit - 1, current_usage = current_usage + 1 WHERE token = $1`,
             [token]
+        );
+        // 向images表中插入数据 user_id,url,created_at,token_id,album_id,update_at
+        await query(
+            `INSERT INTO images (user_id, url, created_at, token_id, album_id, update_at) VALUES ($1, $2, NOW(), $3, $4, NOW())`,
+            [info.user_id, url, info.token_id, albumId]
         );
         return BizResult.success({url: url}, msg);
     } catch (err) {
