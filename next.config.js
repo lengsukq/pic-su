35754c02e7fa682dc86d9fbed8103f3057a1dcdd
lengsukq <lengsukq@foxmail.com>
2024-05-29 13:17:27 +0800

    process.env.RUNTIME

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    serverRuntimeConfig: {
+        runtime: process.env.RUNTIME
+    },
     compiler: {
         // Enables the styled-components SWC transform
         styledComponents: true
