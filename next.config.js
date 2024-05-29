aa4d1458aec4815c47cfbfac02c9d2a81bfb4806
lengsukq <lengsukq@foxmail.com>
2024-05-29 16:00:34 +0800

    新增DATABASE_URL支持

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverRuntimeConfig: {
-        runtime: process.env.RUNTIME
-    },
     compiler: {
         // Enables the styled-components SWC transform
         styledComponents: true
--- utils/db.ts
+++ utils/db.ts
@@ -1,21 +1,41 @@
 // db.ts
 import { Pool } from 'pg';
+
 interface Config {
-    user: string;
-    host: string;
-    database: string;
-    password: string;
+    user?: string;
+    host?: string;
+    database?: string;
+    password?: string;
     port?: number;
+    connectionString?: string;
+    ssl?: {
+        rejectUnauthorized: boolean;
+    };
 }
 
-let configs: Config = {
-    user: process.env.DB_USER as string,
-    host: process.env.DB_HOST as string,
-    database: process.env.DB_NAME as string,
-    password: process.env.DB_PASSWORD as string,
-};
-// 如果process.env.DB_PORT存在则使用process.env.DB_PORT，否则使用默认值5432
-if (process.env.DB_PORT) configs={...configs, port: parseInt(process.env.DB_PORT, 10)};
+let configs: Config = {};
+
+// 如果DATABASE_URL环境变量存在，使用connectionString
+if (process.env.DATABASE_URL) {
+    configs = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false // 如果你使用的是自签名证书或内部证书颁发机构，你可能需要设置这个选项为false
+        }
+    };
+} else {
+    configs = {
+        user: process.env.DB_USER as string,
+        host: process.env.DB_HOST as string,
+        database: process.env.DB_NAME as string,
+        password: process.env.DB_PASSWORD as string,
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+        ssl: {
+            rejectUnauthorized: false // 如果你使用的是自签名证书或内部证书颁发机构，你可能需要设置这个选项为false
+        }
+    };
+}
+
 // 创建 PostgreSQL 连接池
 const pool = new Pool(configs);
 
@@ -26,17 +46,14 @@ export async function query(text: string, params?: any[]) {
         const res = await pool.query(text, params);
         const duration = Date.now() - start;
         console.log('executed query', { text, duration, rows: res.rowCount });
-        // await closePool();
         return res;
-    }catch (e){
+    } catch (e) {
         console.log(e);
         throw e;
     }
-
 }
 
 // 连接池关闭函数
 export async function closePool() {
     await pool.end();
 }
-
